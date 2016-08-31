caf158cda9f9295d18f80c4bc6aa206654f5be26
endy <admin@site15.ru>
2016-09-01 00:43:43 +0500

    fix where in if and update table name

--- src/Database/migrations/20160831235347_items_add_fields.js
+++ src/Database/migrations/20160831235347_items_add_fields.js
@@ -10,8 +10,8 @@ exports.up = function (knex, Promise) {
 exports.down = function (knex, Promise) {
     return Promise.all([
         knex.schema.hasColumn('items', 'cost').then(function (exists) {
-            if (!exists) {
-                return knex.schema.table('users', function (table) {
+            if (exists) {
+                return knex.schema.table('items', function (table) {
                     table.dropColumn('cost');
                 });
             }
